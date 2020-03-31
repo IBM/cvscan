7803f83be2a441816f31f8d83327c762fdcf559a
Jon Huhn <huhnjon@gmail.com>
2020-03-31 11:18:34 -0500

    Don't fail on any resource list error

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -92,7 +92,8 @@ func (s *Scanner) ListAll(namespace string, opts metav1.ListOptions, outDir stri
 		// Ignore MCM resource errors because they require an additional secret
 		// to be provided in the request.
 		if err != nil && !errors.IsNotFound(err) && !errors.IsMethodNotSupported(err) && !((errors.IsBadRequest(err) || errors.IsServiceUnavailable(err)) && strings.HasSuffix(client.APIVersion().Group, ".clusterapi.io")) {
-			return fmt.Errorf("listing %s: %v", name, err)
+			fmt.Printf("Error listing %s: %v", name, err)
+			continue
 		}
 
 		if len(l.Items) == 0 {
